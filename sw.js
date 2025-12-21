f5a979aed4f1a1bb6ea2e013d983e9f0be8e27cd
BILLY <vivaansoni30@gmail.com>
2025-12-21 10:37:08 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE = "peak-cache-v1";
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(["./"]))
-  );
+  self.skipWaiting();
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+
+self.addEventListener("fetch", () => {});
